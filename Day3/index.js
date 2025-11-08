002c652eafc1eac4ce258ea699288d8caab17f37
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-08 23:04:49 +0530

    Authentication

--- Day3/index.js
+++ Day3/index.js
@@ -3,4 +3,24 @@
 // npm i mongoose
 // npm ibcrypt
 
-let express = require
+let express = require('express')
+let mongoose = require('mongoose')
+
+
+let app = express()
+app.use(express.json())
+mongoose.connect('mongodb://127.0.0.1:27017/5thSem')
+.then(() => {console.log('Connected to MongoDB')
+.catch((err) => {console.error('Error connecting to MongoDB:', err)})
+});
+
+app.get('/',(req,res)=>{
+    res.send("hiii")
+
+ })
+
+app.listen(4000,()=>{
+    console.log("server running on port no 4000");
+    
+ })
+
