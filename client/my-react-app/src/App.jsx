a9d69d465ad9e9477f2976f97120f006d49d7a74
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-12 11:40:03 +0530

    OTP Generate

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import SignUp from './SignUp'
-import Login from './Login'
-import { Route, Routes } from 'react-router-dom'
+// import SignUp from './SignUp'
+// import Login from './Login'
+// import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import OTPgenetate from './OTPgenetate'
 const App = () => {
   return (
     <div>
    
-
-      <Routes>
+     <OTPgenetate/>
+      {/* <Routes>
         <Route   path='/' element={<SignUp/>}/>
         <Route   path='/login' element={<Login/>}/>
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
--- client/my-react-app/src/OTPgenetate.jsx
+++ client/my-react-app/src/OTPgenetate.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const OTPgenetate = () => {
   return (
-    <div>
+    <div className='OTPgenerate'>
         <h1>Generate OTP</h1>
-        
-
+        <form action="">
+        <input type="text" placeholder='Enter your Phone Number' />
+        <br />
+        <br />
+        <button type="submit">Generate OTP</button>
+        <br />
+        </form>
     </div>
   )
 }
